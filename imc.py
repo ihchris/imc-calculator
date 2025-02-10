cced6cdc8c4165a5539d0da047070218bcdc3ee7
Christian Anet Afonso de Andrade <90299478+ihchris@users.noreply.github.com>
2025-02-09 22:29:40 -0300

    Refactor IMC calculation and add classification based on BMI ranges

--- imc.py
+++ imc.py
@@ -1,5 +1,19 @@
-peso = input('Peso: ')
-altura = input('Altura: ')
-IMC = (int(peso) / int(altura))
+peso = float(input('Peso (kg): '))
+altura = float(input('Altura (m): '))
+imc = peso / (altura ** 2)
 
-imc = print(f'O seu IMC: {IMC}')
+if imc < 18.5:
+    categoria = "Abaixo do peso"
+elif 18.5 <= imc < 25:
+    categoria = "Peso normal"
+elif 25 <= imc < 30:
+    categoria = "Sobrepeso"
+elif 30 <= imc < 35:
+    categoria = "Obesidade Grau I"
+elif 35 <= imc < 40:
+    categoria = "Obesidade Grau II"
+else:
+    categoria = "Obesidade Grau III"
+
+print(f'\nSeu IMC: {imc:.1f}')
+print(f'Classificação: {categoria}')
